fb31cb4a73083ed80ab7ca381b4640d9235af67d
Maximilian Remming <maximilian.remming@cs.helsinki.fi>
2015-09-14 13:58:00 +0300

    Small work on players

--- InfiJack/src/main/java/logic/players/HumanPlayer.java
+++ InfiJack/src/main/java/logic/players/HumanPlayer.java
@@ -1,21 +1,25 @@
 package logic.players;
 
 import logic.Board;
-import logic.util.Point;
+import logic.Mark;
 
 public class HumanPlayer implements Player {
     private final char mark;
-    private Board board;
+    private final Board board;
 
     public HumanPlayer(char mark) {
         this.mark = mark;
+        this.board = null;
     }
 
     @Override
-    public Point move() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Mark move() {
+        if (this.board == null)
+            throw new Error("Board must be set before calling move()");
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public void setBoard(Board board) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
--- InfiJack/src/main/java/logic/players/Player.java
+++ InfiJack/src/main/java/logic/players/Player.java
@@ -1,8 +1,11 @@
 package logic.players;
 
+import logic.Board;
 import logic.Mark;
 
 public interface Player {
 
     public Mark move();
+
+    public void setBoard(Board board);
 }
