b35286177e0a357922bfe9e39aeb45788d0b4b01
Maximilian Remming <maximilian.remming@cs.helsinki.fi>
2015-10-18 14:12:37 +0300

    Added cli options

--- InfiJack/src/main/java/org/remming/infijack/Main.java
+++ InfiJack/src/main/java/org/remming/infijack/Main.java
@@ -5,14 +5,34 @@ import ui.GUI;
 
 public class Main {
     public static void main(String[] args) {
-        int players = 4;
-        int padding = 1;
+        if (args.length > 2) {
+            printHelp();
+            return;
+        }
         
-        if (args.length >= 1) {
-            players = Integer.parseInt(args[0]);
-        } else if (args.length >= 2) {
-            padding = Integer.parseInt(args[1]);
+        int players = 2;
+        int padding = 1;
+        try {
+            if (args.length >= 1) {
+                players = Integer.parseInt(args[0]);
+                if (players < 2) {
+                    players = 2;
+                }
+            }
+            if (args.length >= 2) {
+                padding = Integer.parseInt(args[1]);
+                if (padding < 1) {
+                    padding = 1;
+                }
+            }
+        } catch (Exception e) {
+            printHelp();
+            return;
         }
         new GUI(new Game(players, padding));
+    }
+    
+    public static void printHelp() {
+        System.out.println("Usage: java -jar InfiJack.jar [players(2-4)] [padding(>0)]");
     }
 }
